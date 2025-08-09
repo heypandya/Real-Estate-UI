df332136fa7c1c0103fc7717b22c7f115778e9c2
heypandya <en.pandiyan@gmail.com>
2025-08-09 21:12:25 +0530

    listpage listing finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import Navbar from "./components/navbar/Navbar";
+import Layout from "./routes/layout/layout";
 import HomePage from "./routes/homePage/homePage";
-import "./layout.scss";
+import ListPage from "./routes/listPage/listPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SinglePage from "./routes/singlePage/singlePage";
+
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/routes/login/login.jsx
@@ -0,0 +1,7 @@
+import "./login.scss";
+
+function Login() {
+  return <div className="login">login</div>;
+}
+
+export default Login;
